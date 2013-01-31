2fa90b9ef954e60f5033a9f54e59019c790c45c6
jgollakner@my.wctc.edu <>
2013-01-30 18:42:48 -0600

    Began Class4Lab

---
+++ src/java/controller/CalculationSelectionController.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author JoeGollakner
+ */
+public class CalculationSelectionController extends HttpServlet {
+    private static final String FINAL_RESULTS = "results.jsp";
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html");
+        
+        String l = request.getParameter("length");
+        String w = request.getParameter("width");
+        
+        
+        
+        
+        
+        RequestDispatcher view =
+                request.getRequestDispatcher(FINAL_RESULTS);
+        view.forward(request, response);
+        
+        
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Main Controller";
+    }// </editor-fold>
+}
---
+++ web/index.jsp
@@ -0,0 +1,30 @@
+<%-- 
+    Document   : index
+    Created on : Jan 30, 2013, 5:46:57 PM
+    Author     : JoeGollakner
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>+-*/ Math Calculations /*-+</title>
+    </head>
+    <body>
+
+            <p>Please select the calculation you wish to perform!</p>   
+            <button onclick="window.location.href='areaForm.html'">Area of a Rectangle</button>
+            <button onclick="window.location.href='circleAreaForm.html'">Area of a Circle</button>
+            <button onclick="window.location.href='missingSide.html'">Find the Missing Side of a Triangle</button>
+            
+        
+                
+        <%-- <h1>Click <a href="areaForm.html">AREA</a> to calculate the area of an object!</h1>
+    </div>
+        <h1>Click <a href="circleAreaForm.html">CIRCLE</a> to calculate the area of a circle!</h1>
+    </div>
+        <h1>Click <a href="missingSide.html">CIRCLE</a> to calculate the area of a circle!</h1>
+        --%>
+</body>
+</html>
